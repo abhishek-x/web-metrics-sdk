f531f7ad8dfed45adccb13a411a9032dda53ef6e
Abhishek Aggarwal <abhishek.aggarwal836@gmail.com>
2024-12-14 02:49:15 +0530

    feat: add core web vitals collection (FCP, LCP, CLS)

--- src/metrics.js
+++ src/metrics.js
@@ -1,17 +1,9 @@
-export function collectFCP(callback) {
-    if (typeof PerformanceObserver === 'function') {
-        const observer = new PerformanceObserver((list) => {
-            const entries = list.getEntriesByName('first-contentful-paint');
-            if (entries.length > 0) {
-                callback(entries[0].startTime);
-            }
-        });
-        try {
-            observer.observe({ type: 'paint', buffered: true });
-        } catch (e) {
-            console.warn('FCP not supported');
-        }
-    } else {
-        console.warn('PerformanceObserver not supported, cannot collect FCP.');
-    }
+import { getFCP, getLCP, getCLS, getFID, getINP } from 'web-vitals';
+
+export function collectCoreWebVitals(callback) {
+    getFCP(metric => callback('FCP', metric.value));
+    getLCP(metric => callback('LCP', metric.value));
+    getCLS(metric => callback('CLS', metric.value));
+    // For now, we won't do FID or INP yet, to keep it simpler at this stage.
+    // They can be added later.
 }
