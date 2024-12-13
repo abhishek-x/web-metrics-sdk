bc17b40e65f2f5f49fdb510934f7e6e6d982d864
Abhishek Aggarwal <abhishek.aggarwal836@gmail.com>
2024-12-14 02:50:56 +0530

    feat: add performance config and integrate core web vitals collection

--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
-import { collectFCP } from './metrics.js';
+import { collectCoreWebVitals } from './metrics.js';
 
 class WebMetricsSDK {
     constructor(config = {}) {
         this.config = {
             endpoint: config.endpoint || null,
             debug: config.debug || false,
+            performance: config.performance || 'none', // new
         };
     }
 
     init() {
-        // Collect FCP and log or send it
-        collectFCP((fcpValue) => {
-            const data = { metric: 'FCP', value: fcpValue };
+        if (this.config.performance === 'coreWebVitals') {
+            collectCoreWebVitals((metric, value) => this.handleMetric(metric, value));
+        } else {
+            if (this.config.debug) console.log('Performance metrics disabled');
+        }
+    }
 
-            if (this.config.debug) {
-                console.log('Collected:', data);
-            }
-
-            if (this.config.endpoint) {
-                // For now, just log that we would send
-                console.log(`Would send data to ${this.config.endpoint}:`, data);
-            }
-        });
+    handleMetric(metric, value) {
+        const data = { metric, value };
+        if (this.config.debug) {
+            console.log('Collected:', data);
+        }
+        if (this.config.endpoint) {
+            // Placeholder send (just logging for now)
+            console.log(`Would send to ${this.config.endpoint}:`, data);
+        }
     }
 }
 
